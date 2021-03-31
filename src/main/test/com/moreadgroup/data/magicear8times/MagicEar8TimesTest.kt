e252f64e0967ed9d7378502ea162d18380a7a7ee
陈朵拉 <conan8chan@yahoo.com>
2021-03-31 22:48:56 +0800

    correct introduction-3.json

--- src/main/test/com/moreadgroup/data/magicear8times/MagicEar8TimesTest.kt
+++ src/main/test/com/moreadgroup/data/magicear8times/MagicEar8TimesTest.kt
@@ -33,7 +33,7 @@ class MagicEar8TimesTest {
         listOf(
             Pair("${srcFolder}/introduction/introduction-1.json", true),
             Pair("${srcFolder}/introduction/introduction-2.json", true),
-            Pair("${srcFolder}/introduction/introduction-3.json", false),
+            Pair("${srcFolder}/introduction/introduction-3.json", true),
 
             Pair("${srcFolder}/primary/primary-1.json", false),
             Pair("${srcFolder}/primary/primary-2.json", false),
