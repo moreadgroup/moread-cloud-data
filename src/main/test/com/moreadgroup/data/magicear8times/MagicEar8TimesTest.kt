918a71fc40b585d089252ded2cdd3251149b9e45
陈朵拉 <conan8chan@yahoo.com>
2021-04-10 13:29:11 +0800

    junior-02.csv

--- src/main/test/com/moreadgroup/data/magicear8times/MagicEar8TimesTest.kt
+++ src/main/test/com/moreadgroup/data/magicear8times/MagicEar8TimesTest.kt
@@ -245,11 +245,11 @@ X-TIMESTAMP-MAP=MPEGTS:126000,LOCAL:00:00:00.000
 //            QuizFileInfo("${srcFolder}/primary/primary-5.csv", "meqzp05",5,"primary","Primary Quiz Fifth"  ,"For Primary Textbook 5","qi-primary-5.csv"),
 //            QuizFileInfo("${srcFolder}/primary/primary-6.csv", "meqzp06",6,"primary","Primary Quiz Sixth"  ,"For Primary Textbook 6","qi-primary-6.csv"),
 //            QuizFileInfo("${srcFolder}/primary/primary-7.csv", "meqzp07",7,"primary","Primary Quiz Seventh","For Primary Textbook 7","qi-primary-7.csv"),
-            QuizFileInfo("${srcFolder}/primary/primary-8.csv", "meqzp08",8,"primary","Primary Quiz Eighth" ,"For Primary Textbook 8","qi-primary-8.csv"),
+//            QuizFileInfo("${srcFolder}/primary/primary-8.csv", "meqzp08",8,"primary","Primary Quiz Eighth" ,"For Primary Textbook 8","qi-primary-8.csv"),
 //
 //            QuizFileInfo("${srcFolder}/junior/junior-01.csv", "meqzj01",1, "junior","Junior Quiz First"  ,"For Junior Textbook 01","qi-junior-01.csv"),
 //            QuizFileInfo("${srcFolder}/junior/junior-02.csv", "meqzj02",2, "junior","Junior Quiz Second" ,"For Junior Textbook 02","qi-junior-02.csv"),
-//            QuizFileInfo("${srcFolder}/junior/junior-03.csv", "meqzj03",3, "junior","Junior Quiz Third"  ,"For Junior Textbook 03","qi-junior-03.csv"),
+            QuizFileInfo("${srcFolder}/junior/junior-03.csv", "meqzj03",3, "junior","Junior Quiz Third"  ,"For Junior Textbook 03","qi-junior-03.csv"),
 //            QuizFileInfo("${srcFolder}/junior/junior-04.csv", "meqzj04",4, "junior","Junior Quiz Fourth" ,"For Junior Textbook 04","qi-junior-04.csv"),
 //            QuizFileInfo("${srcFolder}/junior/junior-05.csv", "meqzj05",5, "junior","Junior Quiz Fifth"  ,"For Junior Textbook 05","qi-junior-05.csv"),
 //            QuizFileInfo("${srcFolder}/junior/junior-06.csv", "meqzj06",6, "junior","Junior Quiz Sixth"  ,"For Junior Textbook 06","qi-junior-06.csv"),
