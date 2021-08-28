0d0a7f02f1d2eba42acd2029abe8d8f9afa16060
陈朵拉 <conan8chan@yahoo.com>
2021-08-28 20:20:31 +0800

    cnword stroke fix

--- src/test/kotlin/com/moreadgroup/data/chinese/CnWordFixTest.kt
+++ src/test/kotlin/com/moreadgroup/data/chinese/CnWordFixTest.kt
@@ -18,9 +18,9 @@ import java.nio.file.Paths
  */
 class CnWordFixTest {
 
-    private val ROOT_FOLDER = "/Users/CC/github/moreadgroup/moread-cloud-data/src/main/resources/"
+//    private val ROOT_FOLDER = "/Users/CC/github/moreadgroup/moread-cloud-data/src/main/resources/"
 
-    //    val ROOT_FOLDER = "/Users/conanchen/github/moreadgroup/moread-cloud-data/src/main/resources/"
+        val ROOT_FOLDER = "/Users/conanchen/github/moreadgroup/moread-cloud-data/src/main/resources/"
     private val GRAPHQL_SERVER = "http://localhost:18060/domain-task/graphql"
 
     private val srcFolder = ROOT_FOLDER + "chinese/"
