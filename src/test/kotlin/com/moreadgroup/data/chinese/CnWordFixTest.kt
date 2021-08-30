06c1ccd99529b8edacf1541066290afb1fdcb114
陈朵拉 <conan8chan@yahoo.com>
2021-08-30 23:14:30 +0800

    cnword stroke fix

--- src/test/kotlin/com/moreadgroup/data/chinese/CnWordFixTest.kt
+++ src/test/kotlin/com/moreadgroup/data/chinese/CnWordFixTest.kt
@@ -91,7 +91,7 @@ class CnWordFixTest {
 
     private fun getCnWordExplanation(it: String, xinhuaWordMap: Map<String, XinhuaWord>): String? {
 
-        val result =  xinhuaWordMap.get(it)?.explanation?.replace(Regex("[\na-zA-Zāáǎàēéěèīíǐìōóǒòūúǔùǖǘǚǜ]+"),"")
+        val result =  xinhuaWordMap.get(it)?.explanation?.replace(Regex("[\na-zA-Zāáǎàēéěèīíǐìōóǒòūúǔùǖǘǚǜ]+"),"")?.replace(",","，")
 
 
 
